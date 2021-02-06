26c0d10eb41f2a28b46cdfb7c030e134f73a59aa
PickleEric <60155510+PickleEric@users.noreply.github.com>
2021-02-05 22:44:27 -0600

    Test student list still has 1 failure and 2 errors

--- Lab4/studentlists.py
+++ Lab4/studentlists.py
@@ -17,8 +17,8 @@ class ClassList:
         self.class_list = []
         self.max_students = max_students
 
-        if self.max_students <= 0:
-            raise StudentError("Please use a positive number")
+        if self.max_students < 0:
+            raise StudentError
 
         # TODO raise an exception - StudentError - if max_students is zero or negative. 
         # Write test to confirm.
@@ -57,9 +57,12 @@ class ClassList:
 
     ## TODO add a method called is_class_full.
     # This should return True or False to indicate if the class is full.
-    def is_class_full(self,max_students):
+    def is_class_full(self, max_students):
         while self.class_list == max_students:
             return True
+        else:
+            return False 
+
 
     def __str__(self):
         return ", ".join(self.class_list)
--- Lab4/test_studentlists.py
+++ Lab4/test_studentlists.py
@@ -134,8 +134,27 @@ class TestStudentLists(TestCase):
     ## TODO write a test for your new is_class_full method when the class is full. 
     # use assertTrue.
     def test_if_class_is_full_method(self):
-        test_class = ClassList(10)
+        test_class = ClassList(5)
+
+        result = test_class.is_class_full()
+
+        self.assertTrue(result)
     ## TODO write a test for your new is_class_full method for when is empty, 
     # and when it is not full. Use assertFalse.
+    def test_if_class_is_empty_method(self):
+        test_class = ClassList(0)
+
+        result = test_class.is_class_full()
+
+        self.assertFalse(result)
+
+    def test_if_class_is_zero(self):
+        with self.assertRaises(StudentError):
+            test_class = ClassList(0)
+
+    def test_if_class_is_negative(self):
+        with self.assertRaises(StudentError):
+            test_class = ClassList(-1)
+
 if __name__ == '__main__':
     unittest.main()
