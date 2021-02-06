4310f39e82147fbc54d15322599db28d62a6422e
PickleEric <60155510+PickleEric@users.noreply.github.com>
2021-02-05 22:55:12 -0600

    camelCase tests added

--- Lab1/test_camelcase.py
+++ Lab1/test_camelcase.py
@@ -1,8 +1,29 @@
 import camelCase
 from unittest import TestCase
-
+"""
+ assertTrue
+ assertFalse
+ assertIsNone
+ assertIsNotNone
+ assertIn
+ assertNotIn
+"""
 class TestCamelCase(TestCase):
 
     def test_camelcase_sentence(self):
 
         self.assertEqual('helloWorld', camelCase.camel_case('Hello World'))
+
+    def test_a_single_word_used(self):
+        bad_string = 'word'
+
+        result = camelCase.camel_case(bad_string)
+
+        self.assertFalse(result)
+
+    def test_if_left_blank(self):
+        bad_string = ''
+
+        result = camelCase.camel_case(bad_string)
+
+        self.assertTrue(result)
